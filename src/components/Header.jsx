c511f7b5058674df6bd3ada0df3eeb116f77098b
Thomas Marques <tomasmarquesdacosta@gmail.com>
2024-02-22 20:46:51 +0100

    ajout de soulignage des liens accueil et a propos lorsqu'on est sur la page correspondante

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '@styles/header.scss';
 import logo from '../assets/logoHeader.png';
 
-function Header(){
+function Header() {
+    const location = useLocation();
+
+    // Récupérer le nom de la page à partir de l'URL
+    const currentPage = location.pathname.split('/')[1];
+
     return (
         <header>
             <div>
-        <img src={logo} alt="logo" />
+                <img src={logo} alt="logo" />
             </div>
             <nav className='navbar'>
-                <Link to="/">Accueil</Link>
-                <Link to="/a-propos">A Propos</Link>
+                <Link to="/" className={currentPage === '' ? 'active' : ''}>Accueil</Link>
+                <Link to="/a-propos" className={currentPage === 'a-propos' ? 'active' : ''}>A Propos</Link>
             </nav>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
